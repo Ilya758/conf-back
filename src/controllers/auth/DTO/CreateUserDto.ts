f3aa20f843aa87e689c180becf15889e9d389bb2
Illia Skaryna <subscriber6436@gmail.com>
2023-02-02 22:53:48 +0300

    feat: implement CreateUserDto

---
+++ src/controllers/auth/DTO/CreateUserDto.ts
@@ -0,0 +1,21 @@
+import { IsString, Length } from 'class-validator';
+
+export default class CreateUserDto {
+  @IsString({
+    message: 'Username must be string',
+  })
+  public username: string;
+
+  @IsString({
+    message: 'Password must be string',
+  })
+  @Length(8, 20, {
+    message: 'Password length must be in range (8, 20) symbols',
+  })
+  public password: string;
+
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+}
